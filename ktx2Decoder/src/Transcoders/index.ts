5697f92dfb9b6199165959231ff7160e8373b47c
dreamswiki <47926895+dreamswiki@users.noreply.github.com>
2020-09-11 19:10:52 -0400

    Removed everything but playground and NME

--- ktx2Decoder/src/Transcoders/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./liteTranscoder";
-export * from "./liteTranscoder_UASTC_ASTC";
-export * from "./liteTranscoder_UASTC_BC7";
-export * from "./mscTranscoder";
--- src/Shaders/ShadersInclude/prePassDeclaration.fx
+++
@@ -1,5 +0,0 @@
-#ifdef PREPASS
-#extension GL_EXT_draw_buffers : require
-layout(location = 0) out vec4 glFragData[{X}];
-vec4 gl_FragColor;
-#endif
